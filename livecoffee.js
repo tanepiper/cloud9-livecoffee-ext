2c4c6457dee0dce01fc296e3f3cc52ffa8def2ed
matt <matt.pardee@gmail.com>
2011-04-12 11:58:53 -0700

    Fixed keybindings, moved exec code to separate function, formatting

--- livecoffee.js
+++ livecoffee.js
@@ -1,95 +1,116 @@
-(function() {
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  define(function(require, exports, module) {
-    var CoffeeScript, editors, ext, ide, markup, util;
-    ide = require('core/ide');
-    ext = require('core/ext');
-    util = require('core/util');
-    editors = require('ext/editors/editors');
-    markup = require('text!ext/livecoffee/livecoffee.xml');
-    CoffeeScript = require('ext/livecoffee/coffeescript');
-    return ext.register('ext/livecoffee/livecoffee', {
-      name: 'LiveCoffee',
-      dev: 'Tane Piper',
-      type: ext.GENERAL,
-      alone: true,
-      markup: markup,
-      commands: {
-        'livecoffee': {
-          hint: 'Compile the current coffeescript document'
+define(function(require, exports, module) {
+
+var ide          = require('core/ide');
+var ext          = require('core/ext');
+var util         = require('core/util');
+var editors      = require('ext/editors/editors');
+var markup       = require('text!ext/livecoffee/livecoffee.xml');
+var CoffeeScript = require('ext/livecoffee/coffeescript');
+
+return ext.register('ext/livecoffee/livecoffee', {
+    name   : 'LiveCoffee',
+    dev    : 'Tane Piper',
+    type   : ext.GENERAL,
+    alone  : true,
+    markup : markup,
+
+    commands : {
+        "livecoffee": {hint: "Compile the current coffeescript document"}
+    },
+
+    hotitems : {},
+    nodes    : [],
+
+    hook : function() {
+        var _self = this;
+
+        this.nodes.push(ide.mnuEdit.appendChild(new apf.divider()));
+        this.nodes.push(
+            mnuEdit.appendChild(new apf.item({
+                caption: 'LiveCoffee',
+                onclick: function() {            
+                    _self.livecoffee();
+                }
+            }))
+        );
+        
+        this.hotitems["livecoffee"] = [this.nodes[1]];
+    },
+
+    livecoffee : function() {
+        var editor;
+        ext.initExtension(this);
+        this.compile();
+        this.liveCoffeeOutput.show();
+        
+        var _self = this;
+        if (this.liveCoffeeOutput.visible) {
+            editor = editors.currentEditor;
+            editor.ceEditor.addEventListener('keyup', function() {
+                return _self.compile();
+            })
         }
-      },
-      hotkeys: {
-        'compile': 1
-      },
-      hotitems: {},
-      nodes: [],
-      compile: function() {
+    },
+    
+    compile : function() {
         var bare, compiledJS, doc, editor, value;
         editor = editors.currentEditor;
         doc = editor.getDocument();
         value = doc.getValue();
         compiledJS = '';
+        
         try {
-          bare = this.liveCoffeeOptCompileBare.checked;
-          compiledJS = CoffeeScript.compile(value, {
-            bare: bare
-          });
-          this.liveCoffeeCodeOutput.setValue(compiledJS);
-          if (this.liveCoffeeOptMatchLines.checked) {
-            this.liveCoffeeCodeOutput.$editor.gotoLine(editor.ceEditor.line);
-          }
-          if (this.liveCoffeeOptCompileNodes.checked) {
-            this.liveCoffeeNodeOutput.setValue(CoffeeScript.nodes(value));
-          }
-          if (this.liveCoffeeOptCompileTokens.checked) {
-            this.liveCoffeeTokenOutput.setValue(CoffeeScript.tokens(value));
-          }
-        } catch (exp) {
-          this.liveCoffeeCodeOutput.setValue(exp.message);
-        }
-      },
-      hook: function() {
-        this.nodes.push(ide.mnuEdit.appendChild(new apf.divider()));
-        this.nodes.push(ide.mnuEdit.appendChild(new apf.item({
-          caption: 'LiveCoffee',
-          onclick: __bind(function() {
-            var editor;
-            ext.initExtension(this);
-            this.compile();
-            this.liveCoffeeOutput.show();
-            if (this.liveCoffeeOutput.visible) {
-              editor = editors.currentEditor;
-              editor.ceEditor.addEventListener('keyup', __bind(function() {
-                return this.compile();
-              }, this));
+            bare = this.liveCoffeeOptCompileBare.checked;
+            compiledJS = CoffeeScript.compile(value, {
+                bare: bare
+            });
+        
+            this.liveCoffeeCodeOutput.setValue(compiledJS);
+            
+            if (this.liveCoffeeOptMatchLines.checked) {
+                this.liveCoffeeCodeOutput.$editor.gotoLine(editor.ceEditor.line);
             }
-          }, this)
-        })));
-        this.hotitems['livecoffee'] = [this.nodes[1]];
-      },
-      init: function(amlNode) {
-        liveCoffeeOptCompileBare.addEventListener('click', __bind(function() {
-          return this.compile();
-        }, this));
+            
+            if (this.liveCoffeeOptCompileNodes.checked) {
+                this.liveCoffeeNodeOutput.setValue(CoffeeScript.nodes(value));
+            }
+            
+            if (this.liveCoffeeOptCompileTokens.checked) {
+                this.liveCoffeeTokenOutput.setValue(CoffeeScript.tokens(value));
+            }
+        } catch (exp) {
+            this.liveCoffeeCodeOutput.setValue(exp.message);
+        }
+    },
+
+    init : function(amlNode) {
+        var _self = this;
+
+        liveCoffeeOptCompileBare.addEventListener('click', function() {
+            return _self.compile();
+        });
+
         this.liveCoffeeOptCompileBare = liveCoffeeOptCompileBare;
-        liveCoffeeOptCompileNodes.addEventListener('click', __bind(function() {
-          if (liveCoffeeOptCompileNodes.checked) {
-            this.liveCoffeeNodes.enable();
-            return this.compile();
-          } else {
-            return liveCoffeeNodes.disable();
-          }
-        }, this));
+
+        liveCoffeeOptCompileNodes.addEventListener('click', function() {
+            if (liveCoffeeOptCompileNodes.checked) {
+                _self.liveCoffeeNodes.enable();
+                return _self.compile();
+            } else {
+                return liveCoffeeNodes.disable();
+            }
+        });
+        
         this.liveCoffeeOptCompileNodes = liveCoffeeOptCompileNodes;
-        liveCoffeeOptCompileTokens.addEventListener('click', __bind(function() {
-          if (liveCoffeeOptCompileTokens.checked) {
-            this.liveCoffeeTokens.enable();
-            return this.compile();
-          } else {
-            return this.liveCoffeeTokens.disable();
-          }
-        }, this));
+        liveCoffeeOptCompileTokens.addEventListener('click', function() {
+            if (liveCoffeeOptCompileTokens.checked) {
+                _self.liveCoffeeTokens.enable();
+                return _self.compile();
+            } else {
+                return _self.liveCoffeeTokens.disable();
+            }
+        });
+        
         this.liveCoffeeOptCompileTokens = liveCoffeeOptCompileTokens;
         this.liveCoffeeOptMatchLines = liveCoffeeOptMatchLines;
         liveCoffeeCodeOutput.syntax = 'javascript';
@@ -101,24 +122,28 @@
         liveCoffeeTokens.disable();
         this.liveCoffeeTokens = liveCoffeeTokens;
         this.liveCoffeeTokenOutput = liveCoffeeTokenOutput;
-      },
-      enable: function() {
+    },
+
+    enable : function() {
         this.nodes.each(function(item) {
-          item.enable();
+            item.enable();
         });
-      },
-      disable: function() {
+    },
+    
+    disable : function() {
         this.nodes.each(function(item) {
-          item.disable();
+            item.disable();
         });
-      },
-      destroy: function() {
+    },
+    
+    destroy : function() {
         this.nodes.each(function(item) {
-          item.destroy(true, true);
+            item.destroy(true, true);
         });
+
         this.nodes = [];
         this.liveCoffeeOutput.destroy(true, true);
-      }
-    });
-  });
-}).call(this);
+    }
+});
+
+});
